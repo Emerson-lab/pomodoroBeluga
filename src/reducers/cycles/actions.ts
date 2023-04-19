a49131314a289d5cd57eb87c1897af306b9d68ab
Emerson Trindade <trinddade29@gmail.com>
2023-04-19 11:53:53 -0300

    immer add

--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -11,7 +11,7 @@ export function addNewCycleAction(newCycle: Cycle) {
 }
 
 export function markCurrentCycleAsFinishedAction() {
-  return { type: ActionTypes.ADD_NEW_CYCLE}
+  return { type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED}
 }
 
 export function interruptCurrentCycleAction() {
--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { ActionTypes } from "./actions";
 
 
@@ -21,35 +22,75 @@ export function cyclesReducer(state: CyclesState, action: any) {
 
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id
-      }
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map(cycle => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null
-      }
+      //modo tradicional: respeitando as regras de imutabilidade do react
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id
+      // }
+      //----------------------------------------------------------------//
+      //metodo com a lib immer-js
+      return produce(state, draft => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id
+      })
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      //modo tradicional: respeitando as regras de imutabilidade do react
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map(cycle => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptedDate: new Date() }
+      //     } else {
+      //       return cycle
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      //----------------------------------------------------------------//
+      //metodo com a lib immer-js
+      const curretnCycleIndex = state.cycles.findIndex(cycle => {
+        return cycle.id === state.activeCycleId
+      });
+
+      if (curretnCycleIndex < 0) {
+        return state
+      };
+
+      return produce(state, draft => {
+        draft.activeCycleId = null;
+        draft.cycles[curretnCycleIndex].interruptedDate = new Date();
+      })
+    }
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map(cycle => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null
-      }
+      //modo tradicional: respeitando as regras de imutabilidade do react
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map(cycle => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, finishedDate: new Date() }
+      //     } else {
+      //       return cycle
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      //----------------------------------------------------------------//
+      //metodo com a lib immer-js
+      const curretnCycleIndex = state.cycles.findIndex(cycle => {
+        return cycle.id === state.activeCycleId
+      });
+
+      if (curretnCycleIndex < 0) {
+        return state
+      };
+
+      return produce(state, draft => {
+        draft.activeCycleId = null;
+        draft.cycles[curretnCycleIndex].finishedDate = new Date();
+      })
     default: return state
   }
 
